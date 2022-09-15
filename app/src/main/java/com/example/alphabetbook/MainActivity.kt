515cd80338d217223950d9e0ec96dfdbbe900ceb
Khanya2000 <mqlkha002@myuct.ac.za>
2022-09-15 21:21:29 +0200

    Exit button coded.

--- app/src/main/java/com/example/alphabetbook/MainActivity.kt
+++ app/src/main/java/com/example/alphabetbook/MainActivity.kt
@@ -2,10 +2,23 @@ package com.example.alphabetbook
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.widget.Button
+import androidx.appcompat.app.AlertDialog
 
 class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+
+        val btnExit = findViewById<Button>(R.id.exit)
+        btnExit.setOnClickListener {
+            val builder = AlertDialog.Builder(this@MainActivity)
+            builder.setTitle("Exit Application")
+            builder.setMessage("Are you sure you want to exit?")
+            builder.setPositiveButton("Yes") { dialogInterface, i -> finish() }
+            builder.setNegativeButton("No") { dialogInterface, i -> dialogInterface.dismiss() }
+            val dialog = builder.create()
+            dialog.show()
+        }
     }
 }
